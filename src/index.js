ff1aaada5690a66412547567b44c3e2b69815a5c
RoyJumah <royjumah17@gmail.com>
2022-12-30 13:14:07 +0300

    Add style for Movie App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 // eslint-disable-next-line import/no-unresolved
-import _ from 'lodash';
-import './style.css';
+
+import './css/style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
   return element;
 }
